58edbfb4ec772e42fbebe04cfde9ebbe1fd9224d
Ofuzor chukwuemeke <uniben2018@gmail.com>
2023-12-09 06:36:41 +0100

    added mongo database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import configuration from './config/config'
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
+const config = configuration()
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [ConfigModule.forRoot({
+    load:[configuration],
+    isGlobal:true
+  })],
+  controllers: [AppController],  
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/config/config.ts
@@ -0,0 +1,4 @@
+export default () => ({
+    port: 5000,
+    mongodburl: "mongodb+srv://ofuzor:ofuzor2018@cluster0.qjl8f.mongodb.net/ecommercee?retryWrites=true&w=majority"
+})
